c193e05c8136fbe21cf326b90ea9fc08fbae141e
Mingfeng Yang <mingfeng.yang@ff0000.com>
2012-08-09 15:12:37 -0700

    added all config files under default-configs

--- fab_deploy/joyent/pgbouncer.py
+++ fab_deploy/joyent/pgbouncer.py
@@ -19,15 +19,20 @@ class PGBouncerInstall(Task):
         sudo('pkg_add py27-psycopg2')
 
         with cd('/tmp'):
-            run('wget %s' %src)
+            run('wget %s' %self.pgbouncer_src)
             sudo('pkg_add %s' %self.pkg_name)
 
         sudo('mkdir -p /etc/pgbouncer')
-        dirname = os.path.join(env.deploy_path, 'pgbouncer')
-        put(dirname, '/etc/', use_sudo=True)
+        mkauth_script = os.path.join(env.configs_dir, 'mkauth.py')
+        pgbouncer_config = os.path.join(env.configs_dir, 'pgbouncer.ini')
+        svc_method = os.path.join(env.configs_dir, 'pgbouncer.xml')
+        put(mkauth_script, '/etc/pgbouncer', use_sudo=True)
+        put(pgbouncer_config, '/etc/pgbouncer', use_sudo=True)
+        put(svc_method, '/etc/pgbouncer', use_sudo=True)
+
         with cd('/etc/pgbouncer'):
             sudo('python mkauth.py userlist.txt "user=postgres"')
-        # postgres should be the owner of these files
+        # postgres should be the owner of these config files
         sudo('chown -R postgres:postgres /etc/pgbouncer')
 
         # pgbouncer won't run smoothly without these directories
@@ -38,9 +43,9 @@ class PGBouncerInstall(Task):
 
         # set up log
         sudo('logadm -C 3 -p1d -c -w /var/log/pgbouncer/pgbouncer.log -z 1')
-        run('svccfg import /srv/active/deploy/pgbouncer/pgbouncer.xml')
+        run('svccfg import /etc/pgbouncer/pgbouncer.xml')
 
-        # run as postgres
-        run('sudo su postgres -c "/opt/pkg/bin/pgbouncer -d /etc/pgbouncer/pgbouncer.ini"')
+        # start pgbouncer
+        sudo('svcadm enable pgbouncer')
 
 setup = PGBouncerInstall()
