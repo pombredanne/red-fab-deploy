db31031be8718fbc02ff7e6ea3708105e552797f
Mingfeng Yang <mingfeng.yang@ff0000.com>
2012-08-09 13:06:01 -0700

    db replication

--- fab_deploy/joyent/setup.py
+++ fab_deploy/joyent/setup.py
@@ -1,6 +1,11 @@
-import sys
-from fabric.api import task, run, sudo, execute, env
+import sys, os
+from time import sleep
+from uuid import uuid4
+from fabric.api import task, run, sudo, execute, env, local, settings
 from fabric.tasks import Task
+from fabric.contrib.files import append, sed, exists, contains
+from fabric.operations import get, put
+from fabric.context_managers import cd
 
 from fab_deploy import functions
 
@@ -188,6 +193,149 @@ class DBSetup(BaseSetup):
         self._save_config()
         execute('postgres.setup')
 
+class ReplicationSetup(BaseSetup):
+    """
+    Set up master-slave streaming replication: slave node
+    """
+
+    name = 'db_slave'
+
+    def _get_data_dir(self, db_version):
+        return os.path.join('/var', 'pgsql', 'data%s' %db_version)
+
+    def _get_master_db_version(self, master):
+        command = ("ssh %s psql --version | head -1 | awk '{print $3}'" %master)
+        version_string = local(command, capture=True)
+        version = ''.join(version_string.split('.')[:2])
+
+        return version
+
+    def _setup_parameter(self, file, **kwargs):
+        for key, value in kwargs.items():
+            origin = "#%s =" %key
+            new = "%s = %s" %(key, value)
+            sudo('sed -i "/%s/ c\%s" %s' %(origin, new, file))
+
+    def _setup_archive_dir(self, data_dir):
+        archive_dir = os.path.join(data_dir, 'wal_archive')
+        sudo("mkdir -p %s" %archive_dir)
+        sudo("chown postgres:postgres %s" %archive_dir)
+
+    def _setup_recovery_conf(self, master_ip, password, data_dir):
+        wal_dir = os.path.join(data_dir, 'wal_archive')
+        recovery_conf = os.path.join(data_dir, 'recovery.conf')
+
+        txts = (("standby_mode = 'on'\n") +
+                ("primary_conninfo = 'host=%s " %master_ip) +
+                    ("port=5432 user=replicator password=%s'\n" %password) +
+                ("trigger_file = '/tmp/pgsql.trigger'\n") +
+                ("restore_command = 'cp -f %s/%s </dev/null'\n"
+                    %(wal_dir, '%f %p')) +
+                ("archive_cleanup_command = 'pg_archivecleanup %s %s'\n"
+                    %(wal_dir, "%r")))
+
+        sudo('touch %s' %recovery_conf)
+        append(recovery_conf, txts, use_sudo=True)
+        sudo('chown postgres:postgres %s' %recovery_conf)
+
+    def _setup_postgres_config(self, data_dir, config):
+        postgres_conf = os.path.join(data_dir, 'postgresql.conf')
+
+        if exists(postgres_conf, use_sudo=True):
+            self._setup_parameter(postgres_conf, **config)
+        else:
+            print ('Could not find %s' %postgres_conf)
+            sys.exit()
+
+    def _setup_hba_config(self, data_dir, hba_txt):
+        hba_conf= os.path.join(data_dir, 'pg_hba.conf')
+        append(hba_conf, hba_txt, use_sudo=True)
+
+    def _ssh_key_exchange(self, master, slave, data_dir):
+        rsa_file = os.path.join('/tmp', 'id_rsa')
+        pub_rsa = os.path.join('/tmp', 'id_rsa.pub')
+        local('ssh-keygen -t rsa -f %s -N ""' %rsa_file)
+
+        for host in [master, slave]:
+            with settings(host_string=host):
+                ssh_dir = '/var/pgsql/.ssh'
+                sudo('mkdir -p %s' %ssh_dir)
+                with cd(ssh_dir):
+                    put(rsa_file, 'id_rsa', use_sudo=True)
+                    put(pub_rsa, 'id_rsa.pub', use_sudo=True)
+                    sudo('cat %s > authorized_keys'
+                         %os.path.join(ssh_dir, 'id_rsa.pub'))
+                    sudo('chown -R postgres:postgres %s' %ssh_dir)
+                    sudo('chmod -R og-rwx %s' %ssh_dir)
+
+        local('rm %s %s' %(rsa_file, pub_rsa))
+
+    def run(self):
+        cons = env.config_object.get_list('db-server',
+                                          env.config_object.CONNECTIONS)
+        if len(cons) > 1:
+            print ('Sorry, there are two db-servers in server.ini, and I don\'t'
+                   'know how to setup two master servers')
+            sys.exit()
+        elif len(cons) < 1:
+            print ('I could not find db server in server.ini.'
+                   'Did you set up db server?')
+            sys.exit()
+        else:
+            master = cons[0]
+            master_ip = master.split('@')[1]
+
+        db_version = self._get_master_db_version(master=master)
+        data_dir = self._get_data_dir(db_version)
+        slave = env.host_string
+        slave_ip = slave.split('@')[1]
+        replicator_pass = uuid4().hex
+
+        # execute('postgres.setup',db_version=db_version) #this or next one?
+        sudo("pkg_add postgresql%s-server" %data_dir)
+        sudo('svcadm enable postgresql:pg%s' %db_version) #initilize
+        sudo('svcadm disable postgresql:pg%s' %db_version)
+
+        self._ssh_key_exchange(master, slave, data_dir)
+
+        with settings(host_string=master):
+            sudo('svcadm disable postgresql:pg%s' %db_version)
+            self._setup_archive_dir(data_dir)
+
+            c1 = ('CREATE USER replicator REPLICATION LOGIN ENCRYPTED PASSWORD \'%s\';'
+                  %replicator_pass)
+            run('psql -U postgres -c "%s"' %c1)
+
+            hba_txt = 'host\treplication\treplicator\t%s/32\tmd5' %slave_ip
+            config = {'wal_level': 'hot_standby',
+                      'wal_keep_segments': '32',
+                      'max_wal_senders':   '5',
+                      'archive_mode':      'on',
+                      'archive_command':   '\'cp %s %s/wal_archive/%s\'' %('%p', data_dir, '%f')}
+            self._setup_postgres_config(data_dir=data_dir, config=config)
+            self._setup_hba_config(data_dir=data_dir, hba_txt=hba_txt)
+
+            sudo('svcadm enable postgresql:pg%s' %db_version)
+            sleep(1) # to make sure the server is really started
+
+            run('psql -U postgres -c "SELECT pg_start_backup(\'backup\', true)"')
+            run('sudo su - postgres -c "rsync -av --exclude postmaster.pid --exclude pg_xlog %s/ postgres@%s:%s/"' %(data_dir, slave_ip, data_dir))
+            run('psql -U postgres -c "SELECT pg_stop_backup()"')
+
+        config = {'wal_level':      'hot_standby',
+                  'hot_standby':    'on',}
+        self._setup_postgres_config(data_dir=data_dir, config=config)
+        self._setup_archive_dir(data_dir)
+        self._setup_recovery_conf(master_ip=master_ip,
+                                  password=replicator_pass, data_dir=data_dir)
+        hba_txt = ('host\treplication\treplicator\t%s/32\tmd5\n'
+                   'host\treplication\treplicator\t%s/32\tmd5'
+                   %(slave_ip,master_ip))
+        self._setup_hba_config(data_dir=data_dir, hba_txt=hba_txt)
+        sudo('svcadm enable postgresql:pg%s' %db_version)
+
+        print('password for replicator on master is %s' %replicator_pass)
+
 
 class DevSetup(AppSetup):
     """
@@ -211,3 +359,4 @@ app_server = AppSetup()
 lb_server = LBSetup()
 dev_server = DevSetup()
 db_server = DBSetup()
+db_slave = ReplicationSetup()
