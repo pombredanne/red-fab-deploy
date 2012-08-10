a837f74a800d65b5496f8758dada498cd66845b9
Mingfeng Yang <mingfeng.yang@ff0000.com>
2012-08-09 19:22:59 -0700

    use in house password script

--- fab_deploy/joyent/pgbouncer.py
+++ fab_deploy/joyent/pgbouncer.py
@@ -1,5 +1,7 @@
 import os
-from fabric.api import run, sudo, env
+from time import sleep
+from cStringIO import StringIO
+from fabric.api import run, sudo, env, hide, local
 from fabric.context_managers import cd
 from fabric.operations import get, put
 from fabric.tasks import Task
@@ -13,27 +15,67 @@ class PGBouncerInstall(Task):
 
     pgbouncer_src = 'http://pkgsrc.smartos.org/packages/SmartOS/2012Q2/databases/pgbouncer-1.4.2.tgz'
     pkg_name = 'pgbouncer-1.4.2.tgz'
+    config_dir = '/etc/opt/pkg'
+
+    config = {
+        '*':              'host=127.0.0.1',
+        'logfile':        '/var/log/pgbouncer/pgbouncer.log',
+        'pidfile':        '/var/run/pgbouncer/pgbouncer.pid',
+        'listen_addr':    '*',
+        'listen_port':    '6432',
+        'unix_socket_dir': '/tmp',
+        'auth_type':      'md5',
+        'auth_file':      '%s/userlist.txt' %config_dir,
+        'pool_mode':      'session',
+        'admin_users':    'postgres',
+        'stats_users':    'postgres',
+        }
+
+    def _setup_parameter(self, file, **kwargs):
+        for key, value in kwargs.items():
+            origin = "%s =" %key
+            new = "%s = %s" %(key, value)
+            sudo('sed -i "/%s/ c\%s" %s' %(origin, new, file))
+
+    def _get_all_passwd(self):
+        with hide('output'):
+            string = run('sudo su postgres -c \'psql -c "select usename, '
+                         'passwd from pg_shadow order by 1"\'')
+        f = StringIO(string)
+        f.readline()
+        f.readline()
+        lines = []
+        for line in f.readlines():
+            list = line.split('|')
+            if len(list) >= 2:
+                user = list[0].strip()
+                passwd = list[1].strip()
+                lines.append('"%s" "%s" ""\n' %(user, passwd))
+
+        out = open('/tmp/userlist.txt', 'w')
+        out.write("".join(lines))
+        out.close()
+        put('/tmp/userlist.txt', '%s/userlist.txt' %self.config_dir, use_sudo=True)
+        local('rm /tmp/userlist.txt')
 
     def run(self):
         sudo('pkg_add libevent')
         sudo('pkg_add py27-psycopg2')
+        sudo('mkdir -p /opt/pkg/bin')
+        sudo("ln -sf /opt/local/bin/awk /opt/pkg/bin/nawk")
+        sudo("ln -sf /opt/local/bin/sed /opt/pkg/bin/nbsed")
 
         with cd('/tmp'):
             run('wget %s' %self.pgbouncer_src)
-            sudo('pkg_add %s' %self.pkg_name)
+            # sudo('pkg_add %s' %self.pkg_name)
 
-        sudo('mkdir -p /etc/pgbouncer')
-        mkauth_script = os.path.join(env.configs_dir, 'mkauth.py')
-        pgbouncer_config = os.path.join(env.configs_dir, 'pgbouncer.ini')
         svc_method = os.path.join(env.configs_dir, 'pgbouncer.xml')
-        put(mkauth_script, '/etc/pgbouncer', use_sudo=True)
-        put(pgbouncer_config, '/etc/pgbouncer', use_sudo=True)
-        put(svc_method, '/etc/pgbouncer', use_sudo=True)
+        put(svc_method, self.config_dir, use_sudo=True)
 
-        with cd('/etc/pgbouncer'):
-            sudo('python mkauth.py userlist.txt "user=postgres"')
+        self._setup_parameter('%s/pgbouncer.ini' %self.config_dir, **self.config)
+        self._get_all_passwd()
         # postgres should be the owner of these config files
-        sudo('chown -R postgres:postgres /etc/pgbouncer')
+        sudo('chown -R postgres:postgres %s' %self.config_dir)
 
         # pgbouncer won't run smoothly without these directories
         sudo('mkdir -p /var/run/pgbouncer')
@@ -43,7 +85,7 @@ class PGBouncerInstall(Task):
 
         # set up log
         sudo('logadm -C 3 -p1d -c -w /var/log/pgbouncer/pgbouncer.log -z 1')
-        run('svccfg import /etc/pgbouncer/pgbouncer.xml')
+        run('svccfg import %s/pgbouncer.xml' %self.config_dir)
 
         # start pgbouncer
         sudo('svcadm enable pgbouncer')
