a20dcf15216d7ccf6343fa48f99ba0ac06f618c0
Mingfeng Yang <mingfeng.yang@ff0000.com>
2012-08-13 09:55:46 -0700

    pgbouncer should be part of postgres

--- fab_deploy/joyent/postgres.py
+++ fab_deploy/joyent/postgres.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import sgmllib
+import urllib2
 
-from fabric.api import run, sudo, env, local
+from fabric.api import run, sudo, env, local, hide
 from fabric.contrib.files import append, sed, exists, contains
-from fabric.context_managers import prefix
+from fabric.context_managers import prefix, cd
+from fabric.operations import get, put
 from fabric.tasks import Task
 
 class PostgresInstall(Task):
@@ -94,4 +97,103 @@ class PostgresInstall(Task):
         run("sudo su postgres -c 'createuser -D -S -R -P %s'" %username)
 
 
+class PGBouncerInstall(Task):
+    """
+    Set up PGBouncer on a database server
+    """
+
+    name = 'setup_pgbouncer'
+
+    pgbouncer_src = 'http://pkgsrc.smartos.org/packages/SmartOS/2012Q2/databases/pgbouncer-1.4.2.tgz'
+    pkg_name = 'pgbouncer-1.4.2.tgz'
+    config_dir = '/etc/opt/pkg'
+
+    config = {
+        '*':              'host=127.0.0.1',
+        'logfile':        '/var/log/pgbouncer/pgbouncer.log',
+        'pidfile':        '/var/run/pgbouncer/pgbouncer.pid',
+        'listen_addr':    '*',
+        'listen_port':    '6432',
+        'unix_socket_dir': '/tmp',
+        'auth_type':      'md5',
+        'auth_file':      '%s/pgbouncer.userlist' %config_dir,
+        'pool_mode':      'session',
+        'admin_users':    'postgres',
+        'stats_users':    'postgres',
+        }
+
+    def _setup_parameter(self, file, **kwargs):
+        for key, value in kwargs.items():
+            origin = "%s =" %key
+            new = "%s = %s" %(key, value)
+            sudo('sed -i "/%s/ c\%s" %s' %(origin, new, file))
+
+    def _get_passwd(self, username):
+        with hide('output'):
+            string = run('sudo su postgres -c \'psql -c "select usename, '
+                         'passwd from pg_shadow where usename=\"%s\"'
+                         'order by 1"\'' %username)
+
+        user, passwd = string.split('\n')[2].split('|')
+        user = list[0].strip()
+        passwd = list[1].strip()
+
+        out = open('/tmp/userlist.txt', 'w')
+        __, tmp_name = tempfile.mkstemp()
+        fn = open(tmp_name, 'w')
+        fn.write('"%s" "%s" ""\n' %(user, passwd))
+        fn.close()
+        put(fn, '%s/pgbouncer.userlist'%self.config_dir, use_sudo=True)
+        local('rm %s' %tmp_name)
+
+    def _get_username(self, section=None):
+        cons = env.config_object.get_list(section, env.config_object.CONNECTIONS)
+        names = env.config_object.get_list(section, env.config_object.USERNAME)
+        try:
+            i = cons.index(env.host_string)
+            username = names[i]
+        except:
+            print ('You must first set up a database server on this machine, '
+                   'and create a database user')
+            raise
+        return username
+
+    def run(self, section):
+
+        sudo('pkg_add libevent')
+        sudo('pkg_add py27-psycopg2')
+        sudo('mkdir -p /opt/pkg/bin')
+        sudo("ln -sf /opt/local/bin/awk /opt/pkg/bin/nawk")
+        sudo("ln -sf /opt/local/bin/sed /opt/pkg/bin/nbsed")
+
+        with cd('/tmp'):
+            run('wget %s' %self.pgbouncer_src)
+            sudo('pkg_add %s' %self.pkg_name)
+
+        svc_method = os.path.join(env.configs_dir, 'pgbouncer.xml')
+        put(svc_method, self.config_dir, use_sudo=True)
+
+        self._setup_parameter('%s/pgbouncer.ini' %self.config_dir, **self.config)
+
+        if not section:
+            section = 'db-server'
+        username = self._get_username(section)
+        self._get_passwd(username)
+        # postgres should be the owner of these config files
+        sudo('chown -R postgres:postgres %s' %self.config_dir)
+
+        # pgbouncer won't run smoothly without these directories
+        sudo('mkdir -p /var/run/pgbouncer')
+        sudo('mkdir -p /var/log/pgbouncer')
+        sudo('chown postgres:postgres /var/run/pgbouncer')
+        sudo('chown postgres:postgres /var/log/pgbouncer')
+
+        # set up log
+        sudo('logadm -C 3 -p1d -c -w /var/log/pgbouncer/pgbouncer.log -z 1')
+        run('svccfg import %s/pgbouncer.xml' %self.config_dir)
+
+        # start pgbouncer
+        sudo('svcadm enable pgbouncer')
+
 setup = PostgresInstall()
+setup_pgbouncer = PGBouncerInstall()
