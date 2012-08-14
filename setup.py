cfcd151b064f12c93ae9cffe35e88a7da4db74c2
Mingfeng Yang <mingfeng.yang@ff0000.com>
2012-08-14 13:46:20 -0700

    make version 0.0.3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
 		'fab_deploy.joyent',
 		'fab_deploy.local',
 		],
-	version = 'v0.0.2',
+	version = 'v0.0.3',
     author='RED Interactive Agency',
     author_email='geeks@ff0000.com',
 
